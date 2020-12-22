9d3e1501d8281d8b247606aa6724c00125c0e0ef
马荣杰 <wb-mrj763442@alibaba-inc.com>
2020-12-22 17:33:13 +0800

    修改3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page 2</h1>
+      <h1 className={styles.title}>Page 3</h1>
     </div>
   );
 };
